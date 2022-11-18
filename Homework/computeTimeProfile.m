89ae7ce76c501f8f7f607f76b679ca0f4bd4265c
uzakotim <timauzakov@gmail.com>
2022-11-18 12:40:45 +0100

    recursiveARX

--- Homework/computeTimeProfile.m
+++ Homework/computeTimeProfile.m
@@ -1,37 +1,38 @@
-function [y] = computeTimeProfile(u,Ts)
+function [y] = computeTimeProfile(u,Ts,tau1,tau2,tau3)
     
     
     variance =0.01;
 
     time = 1000;
     T = 0:Ts:time-1;
-    T_all = 0:Ts:3*time-1;
-    tau1 = 10; % for 0 ... 1000
-    tau2 = 20; % for 1000 ... 2000
-    tau3 = 5;  % for 2000 ... 3000
 
+    tau1 = 10;
     tau = tau1;
-    sys = tf(1,[tau^3 3*tau^2 3*tau 1]);
-    dsys1 = c2d(sys,Ts,'tustin');
-    a = [(8*tau^3+12*tau^2+6*tau+1) (-24*tau^3-12*tau^2+6*tau+3) (24*tau^3-12*tau^2-6*tau+3) (-8*tau^3+12*tau^2-6*tau+1)];
-    He = tf(1, a, Ts,'Variable','z^-1');  
-    y1 = lsim(dsys1, u, T')+lsim(He,sqrt(variance)*randn(time,1),T');
+    b1_ = [1 3 3 1]/(8*tau^3+12*tau^2+6*tau+1);
+    a1_ = [(8*tau^3+12*tau^2+6*tau+1) (-24*tau^3-12*tau^2+6*tau+3) (24*tau^3-12*tau^2-6*tau+3) (-8*tau^3+12*tau^2-6*tau+1)]/(8*tau^3+12*tau^2+6*tau+1);
+    c1_ = 1/(8*tau^3+12*tau^2+6*tau+1);
+    dsys1 = tf(b1_,a1_,Ts,'Variable','z^-1');
+    He = tf(c1_, a1_, Ts,'Variable','z^-1');  
+    y1 = lsim(dsys1, u(1:1000), T')+lsim(He,sqrt(variance)*randn(time,1),T');
     
+    tau2 = 20;
     tau = tau2;
-    sys = tf(1,[tau^3 3*tau^2 3*tau 1]);
-    dsys2 = c2d(sys,Ts,'tustin');
-    a = [(8*tau^3+12*tau^2+6*tau+1) (-24*tau^3-12*tau^2+6*tau+3) (24*tau^3-12*tau^2-6*tau+3) (-8*tau^3+12*tau^2-6*tau+1)];
-    He = tf(1, a, Ts,'Variable','z^-1');  
-    y2 = lsim(dsys2, u, T')+lsim(He,sqrt(variance)*randn(time,1),T');
-    y2 = y2+y1(end)*ones(1000,1);
+    b2_ = [1 3 3 1]/(8*tau^3+12*tau^2+6*tau+1);
+    a2_ = [(8*tau^3+12*tau^2+6*tau+1) (-24*tau^3-12*tau^2+6*tau+3) (24*tau^3-12*tau^2-6*tau+3) (-8*tau^3+12*tau^2-6*tau+1)]/(8*tau^3+12*tau^2+6*tau+1);
+    c2_ = 1/(8*tau^3+12*tau^2+6*tau+1);
+    dsys2 = tf(b2_,a2_,Ts,'Variable','z^-1');
+    He = tf(c2_, a2_, Ts,'Variable','z^-1');  
+    y2 = lsim(dsys2, u(1001:2000), T')+lsim(He,sqrt(variance)*randn(time,1),T');
     
+    tau3 = 5;
     tau = tau3;
-    sys = tf(1,[tau^3 3*tau^2 3*tau 1]);
-    dsys3 = c2d(sys,Ts,'tustin');
-    a = [(8*tau^3+12*tau^2+6*tau+1) (-24*tau^3-12*tau^2+6*tau+3) (24*tau^3-12*tau^2-6*tau+3) (-8*tau^3+12*tau^2-6*tau+1)];
-    He = tf(1, a, Ts,'Variable','z^-1');  
-    y3 = lsim(dsys3, u, T')+lsim(He,sqrt(variance)*randn(time,1),T');
-    y3 = y3+y2(end)*ones(1000,1);
+    b3_ = [1 3 3 1]/(8*tau^3+12*tau^2+6*tau+1);
+    a3_ = [(8*tau^3+12*tau^2+6*tau+1) (-24*tau^3-12*tau^2+6*tau+3) (24*tau^3-12*tau^2-6*tau+3) (-8*tau^3+12*tau^2-6*tau+1)]/(8*tau^3+12*tau^2+6*tau+1);
+    c3_ = 1/(8*tau^3+12*tau^2+6*tau+1);
+    
+    dsys3 = tf(b3_,a3_,Ts,'Variable','z^-1');
+    He = tf(c3_, a3_, Ts,'Variable','z^-1');  
+    y3 = lsim(dsys3, u(2001:3000), T')+lsim(He,sqrt(variance)*randn(time,1),T');
     
     y = cat(1,y1,y2,y3);     %% time-profile
 
--- Homework/forgetting.m
+++ Homework/forgetting.m
@@ -3,3 +3,40 @@
 % Time Varying Exponential
 % Restricted
 % Use of prior information
+function [] = forgetting(input,output,initCov,ForgettingFactor,A0,B0,a1_,a2_,a3_,b1_,b2_,b3_,ttl)
+    na = 3;
+    nb = 4;
+    nk = 0;
+    method = 'ForgettingFactor';
+    obj = recursiveARX([na nb nk],A0,B0,'EstimationMethod',method);
+    obj.InitialParameterCovariance = initCov;
+    obj.ForgettingFactor = ForgettingFactor;
+    
+    A = zeros(numel(input),4);
+    B = zeros(numel(input),4);
+    EstimatedOutput = zeros(numel(input),1);
+    for i = 1:numel(input)
+        [A(i,:),B(i,:),EstimatedOutput(i,:)] = step(obj,output(i),input(i));
+    end
+    
+    subplot(2,1,1)
+    plot(1:1:length(input),A);
+    hold on
+    plot([0,1000,1000,2000,2000,3000],[a1_(1,1),a1_(1,1),a2_(1,1),a2_(1,1),a3_(1,1),a3_(1,1)],'b:')
+    plot([0,1000,1000,2000,2000,3000],[a1_(1,2),a1_(1,2),a2_(1,2),a2_(1,2),a3_(1,2),a3_(1,2)],'b:')
+    plot([0,1000,1000,2000,2000,3000],[a1_(1,3),a1_(1,3),a2_(1,3),a2_(1,3),a3_(1,3),a3_(1,3)],'b:')
+    plot([0,1000,1000,2000,2000,3000],[a1_(1,4),a1_(1,4),a2_(1,4),a2_(1,4),a3_(1,4),a3_(1,4)],'b:')
+    hold off
+    ylabel('a(t)')
+    title(ttl);
+    
+    subplot(2,1,2)
+    plot(1:1:length(input),B)
+    hold on
+    plot([0,1000,1000,2000,2000,3000],[b1_(1,1),b1_(1,1),b2_(1,1),b2_(1,1),b3_(1,1),b3_(1,1)],'b:')
+    plot([0,1000,1000,2000,2000,3000],[b1_(1,2),b1_(1,2),b2_(1,2),b2_(1,2),b3_(1,2),b3_(1,2)],'b:')
+    plot([0,1000,1000,2000,2000,3000],[b1_(1,3),b1_(1,3),b2_(1,3),b2_(1,3),b3_(1,3),b3_(1,3)],'b:')
+    plot([0,1000,1000,2000,2000,3000],[b1_(1,4),b1_(1,4),b2_(1,4),b2_(1,4),b3_(1,4),b3_(1,4)],'b:')
+    hold off 
+    ylabel('b(t)')
+end
